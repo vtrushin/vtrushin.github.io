349cec25caabc48f57f0e91547721a820be93d31
Vlad Trushin <monospectr@mail.ru>
2016-09-14 04:19:35 +0300

    Added styles

--- bookmate/index.js
+++ bookmate/index.js
@@ -1,10 +1,6 @@
-let page = 1;
-let perPage = 10;
-
-
 let quotesEl = document.querySelector('#quotes');
 
-function createCover(cover) {
+function createCoverEl(cover) {
 	return `
 		<div class="image">
 			<img src="${cover.url}" width="${150}" height="${cover.height}"/>
@@ -12,7 +8,7 @@ function createCover(cover) {
 	`;
 }
 
-function createQuote(quote) {
+function createQuoteEl(quote) {
 	console.log(quote);
 
 	return `
@@ -28,31 +24,94 @@ function createQuote(quote) {
 	`;
 }
 
-(function load() {
-	let url = `https://bookmate.com/a/4/u/vashukov/m/grouped.json?p=${page}&pp=${perPage}`;
+(function(){
 
-	fetch(url)
-		.then(response => response.json())
-		.then(data => {
-			if (data.length === 0) return;
-			let quotes = [];
+	function createQuote(object) {
+		return {
+			// author: object.document.authors,
+			// book: object.document.title,
+			// cover: object.document.cover,
+			quote: object.content
+		};
+	}
 
-			data.forEach(book => {
-				book.objects.forEach(object => {
-					quotes.push({
-						author: object.document.authors,
-						book: object.document.title,
-						cover: object.document.cover,
-						quote: object.content
+	function loadBookQuotes(user, bookId) {
+		let quotes = [];
+
+		return new Promise((resolve, reject) => {
+			(function load(page) {
+				let url = `https://bookmate.com/a/4/u/${user}/d/${bookId}/m.json?p=${page}&pp=10`;
+
+				fetch(url)
+					.then(response => response.json())
+					.then(objects => {
+						// Все цитаты загружены, сервер возвращает пустой массив
+						if (objects.length === 0) {
+							resolve(quotes);
+							return;
+						}
+
+						quotes.push(...objects.map(createQuote));
+						load(page + 1);
 					});
-				});
-			});
-
-			quotesEl.innerHTML += quotes.map(createQuote).join('');
-
-			page ++;
-			load();
+			})(1);
 		});
-})();
+	}
 
-// function
+	function loadBookmateQuotes(user) {
+		let quotes = [];
+
+		return new Promise((resolve, reject) => {
+			(function load(page) {
+				let url = `https://bookmate.com/a/4/u/${user}/m/grouped.json?p=${page}&pp=10`;
+
+				fetch(url)
+					.then(response => response.json())
+					.then(data => {
+						// Все книги загружены, сервер возвращает пустой массив
+						if (data.length === 0) {
+							resolve(quotes);
+							return;
+						}
+
+						let bookPromises = data.map(bookWrapper => {
+							let bookId = bookWrapper.objects[0].document_uuid;
+							return loadBookQuotes(user, bookId);
+						});
+
+						Promise.all(bookPromises).then(books => {
+							books.forEach(bookQuotes => {
+								quotes.push(...bookQuotes);
+							});
+							load(page + 1);
+						});
+					});
+			})(1);
+		});
+	}
+
+	// nastenkavoronina
+	// vashukov
+
+	// console.log(loadBookmateQuotes('nastenkavoronina'));
+	/*loadBookmateQuotes('vashukov').then(quotes => {
+		console.log(quotes);
+	});*/
+
+	document.addEventListener('DOMContentLoaded', function() {
+		var userNameEl = document.getElementById('user-name');
+		var searchEl = document.getElementById('search');
+		var quotesEl = document.getElementById('quotes');
+
+		searchEl.addEventListener('click', function() {
+				// console.log(userNameEl.value);
+
+			loadBookmateQuotes(userNameEl.value).then(quotes => {
+
+				quotesEl.innerHTML = quotes.map(createQuoteEl).join('');
+
+			});
+		});
+	});
+
+})();
