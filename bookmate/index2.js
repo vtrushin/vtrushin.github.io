17edd1a6a5033f5dd3d703b399fe728fbb6c2b60
Vlad Trushin <monospectr@mail.ru>
2016-09-29 04:45:33 +0300

    Changed script for bookmate

--- bookmate/index2.js
+++ bookmate/index2.js
@@ -1,6 +1,11 @@
+function declensionOfNumber(number, titles) {
+	return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
+}
+
 function createDefaultOptions(size) {
+	let quoteText = declensionOfNumber(size, ['цитата', 'цитаты', 'цитат']);
 	return `
-		<option value="">Все (${size})</option>
+		<option value="">Все ${size} ${quoteText}</option>
 		<option disabled="disabled">———</option>
 	`;
 }
